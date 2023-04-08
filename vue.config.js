4dfd1679b6e9bea185d3e8553e4fe57145613c6e
Andrey Ageev <ageevweb@gmail.com>
2023-04-08 16:40:18 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/pwa-test/'
+  : '/',
+
+  pwa: {
+    themeColor: "#fff",
+    msTileColor: "#fff",
+    name: "ECO-CITY",
+    display: "fullscreen",
+    scope: "",
+    start_url: ".",
+  },
 })
